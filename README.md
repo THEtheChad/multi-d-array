bd8936585ba74f0ecb6449e4898b680909b1c904
THEtheChad <chad.d.elliott@gmail.com>
2013-12-14 21:15:43 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# multi-d-array
+
+A multi dimensional array toolkit for node.
+
+## Getting Started
+Install the module with: `npm install multi-d-array`
+
+```javascript
+var multi_d_array = require('multi-d-array');
+multi_d_array.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 THEtheChad  
+Licensed under the MIT license.
---
+++ test/multi-d-array_test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Multi = require('../lib/multi-d-array.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+// exports['Initialization'] = {
+//   setUp: function(done){
+//     done();
+//   },
+//   'The array': function(test){
+//     test.expect(2);
+
+//     test.equal( Multi( array[d] ).dimensions, d, 'should be ' + d + ' dimensions.' );
+
+//     test.done();
+//   }
+// };
